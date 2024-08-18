aef47db87a6640a55da8a9eba86a67de756b2ea8
Mahad <myounis@aslase.com>
2024-08-18 17:58:27 +0500

    connection with db

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
+  //swagger
   const config = new DocumentBuilder()
     .setTitle('Todo Backend Greeka')
     .setDescription('The Todo Backend Greeka Api Documentation')
     .setVersion('1.0')
     .build();
+
+  //port from env
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(5000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 5000;
+
+  //server listen
+  await app.listen(port);
 }
 bootstrap();
